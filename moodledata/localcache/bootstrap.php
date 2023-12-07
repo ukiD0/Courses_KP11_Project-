87113ddf523af66b4ec00de338be6f8a356b8eee
Valentina Korovkina <valentinaaakkk@yandex.ru>
2023-12-07 10:37:54 +0300

    deleted side panel

--- moodledata/localcache/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ********** This file is generated DO NOT EDIT **********
-$CFG->siteidentifier = 'NHYxkpFFYBoCSVuheFtkC6G5bTlT4XTnkp11courses';
-$CFG->bootstraphash = 'a8ab8dc21928d4894bd0e9d86a226059';
-// Only if the file is not stale and has not been defined.
-if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
-    define('SYSCONTEXTID', 1);
-}
